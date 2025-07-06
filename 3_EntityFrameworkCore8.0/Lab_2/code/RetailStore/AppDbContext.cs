a23b94a9a664b55797028c8256c92a023032e32f
Baishnab Charan Behera <baishnab1708@gmail.com>
2025-07-06 23:45:19 +0530

    Lab2

---
+++ 3_EntityFrameworkCore8.0/Lab_2/code/RetailStore/AppDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using RetailStore.Models;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<Category> Categories => Set<Category>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=RetailStoreDb;Trusted_Connection=True;");
+    }
+}
