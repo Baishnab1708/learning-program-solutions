a0271530cc0c0994f7996bc8c1dbaf7c8ab6f8a5
Baishnab Charan Behera <baishnab1708@gmail.com>
2025-07-07 00:20:08 +0530

    Update Program.cs

--- 3_EntityFrameworkCore8.0/EFCore8.0HOL/code/RetailStore/Program.cs
+++ 3_EntityFrameworkCore8.0/EFCore8.0HOL/code/RetailStore/Program.cs
@@ -10,25 +10,6 @@ internal class Program
     {
         using var context = new AppDbContext();
 
-        // Comment this section if data already inserted
-        /*
-        if (!await context.Categories.AnyAsync())
-        {
-            var electronics = new Category { Name = "Electronics" };
-            var groceries = new Category { Name = "Groceries" };
-
-            await context.Categories.AddRangeAsync(electronics, groceries);
-
-            var product1 = new Product { Name = "Laptop", Price = 75000, Category = electronics };
-            var product2 = new Product { Name = "Rice Bag", Price = 1200, Category = groceries };
-
-            await context.Products.AddRangeAsync(product1, product2);
-            await context.SaveChangesAsync();
-
-            Console.WriteLine("Initial data inserted.");
-        }
-        */
-
         Console.WriteLine("\nAll Products:");
         var products = await context.Products.ToListAsync();
         foreach (var p in products)
